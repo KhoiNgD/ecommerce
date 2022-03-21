45b8a19912c94f51aa54556cafe6bc521b8c2e1c
Khoi Nguyen <ndkhoi.dev@gmail.com>
2022-03-21 10:19:52 +0700

    add react router dom v6

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Headphones } from "@pages/Headphones";
+import { Speakers } from "@pages/Speakers";
+import { Earphones } from "@pages/Earphones";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="headphones" element={<Headphones />} />
+        <Route path="earphones" element={<Earphones />} />
+        <Route path="speakers" element={<Speakers />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
