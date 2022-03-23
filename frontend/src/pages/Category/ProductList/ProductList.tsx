7b797eca8ae2b29196d77ba4bb7d034fc5e8212c
Khoi Nguyen <ndkhoi.dev@gmail.com>
2022-03-23 21:44:34 +0700

    responsive ProductList component

--- frontend/src/pages/Category/ProductList/ProductList.tsx
+++ frontend/src/pages/Category/ProductList/ProductList.tsx
@@ -1,5 +1,32 @@
+import { Product } from "components/Product";
+import styled from "styled-components";
+
 function ProductList() {
-  return <div>Product</div>;
+  return (
+    <Wrapper>
+      <Product />
+      <Product />
+      <Product />
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 160px;
+
+  & *:nth-child(even) {
+    flex-direction: row-reverse;
+
+    @media (max-width: 1100px) {
+      flex-direction: column;
+    }
+  }
+
+  @media (max-width: 1100px) {
+    gap: 120px;
+  }
+`;
 
 export { ProductList };
