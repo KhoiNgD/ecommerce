d391d42d2b878afb3a0f662eebb3fa7948fe3692
Khoi Nguyen <ndkhoi.dev@gmail.com>
2022-03-29 04:33:42 +0700

    serve backend static files

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const path = require("path");
 
 const api = require("./routes/api");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use("/api/v1", api);
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
