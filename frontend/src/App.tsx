6cbf9354f56ccffc533211d7e93993bb2d467b72
Khoi Nguyen <ndkhoi.dev@gmail.com>
2022-03-29 06:46:50 +0700

    wrap App component in react query provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,18 @@ import { Headers } from "components/Headers";
 import { Outlet } from "react-router-dom";
 import GlobalStyles from "components/GlobalStyles/GlobalStyles";
 import { Footer } from "components/Footer";
+import { QueryClient, QueryClientProvider } from "react-query";
 
+const queryClient = new QueryClient();
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Headers />
       <Outlet />
       <Footer />
 
       <GlobalStyles />
-    </>
+    </QueryClientProvider>
   );
 }
 
