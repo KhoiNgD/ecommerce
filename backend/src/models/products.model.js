cfc6563d145e88cdf8a301fd320beaf79f883b9c
Khoi Nguyen <ndkhoi.dev@gmail.com>
2022-03-29 04:54:46 +0700

    get products by category api

--- backend/src/models/products.model.js
+++ backend/src/models/products.model.js
@@ -4,6 +4,11 @@ function getAllProducts() {
   return products;
 }
 
+function getProductsByCategory(category) {
+  return products.filter((product) => product.category === category);
+}
+
 module.exports = {
   getAllProducts,
+  getProductsByCategory,
 };
