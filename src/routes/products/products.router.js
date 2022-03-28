9622bc123bec920fa3360bc174c52827f23bea28
Khoi Nguyen <ndkhoi.dev@gmail.com>
2022-03-28 19:08:43 +0700

    add backend products controller

--- src/routes/products/products.router.js
+++ src/routes/products/products.router.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const { httpGetAllProducts } = require("./products.controller");
 
 const productsRouter = express.Router();
+
+productsRouter.get("", httpGetAllProducts);
 
 module.exports = productsRouter;
