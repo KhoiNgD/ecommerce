f045cd3a0b4bd37822c3cdc4d2316c3dc80546b8
Khoi Nguyen <ndkhoi.dev@gmail.com>
2022-03-31 20:33:46 +0700

    wrap cart provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,16 @@ import { Outlet } from "react-router-dom";
 import GlobalStyles from "components/GlobalStyles/GlobalStyles";
 import { Footer } from "components/Footer";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { CartProvider } from "contexts/cart-context";
 
 const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Headers />
-      <Outlet />
+      <CartProvider>
+        <Outlet />
+      </CartProvider>
       <Footer />
 
       <GlobalStyles />
